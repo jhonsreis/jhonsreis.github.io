e9e0caf626a52944519f697d31ecaf0a2d64c1ed
jhonsreis <jhonsreis@gmail.com>
2023-02-17 23:13:26 -0300

    Jhonatec pulando e abaixando

--- joguim/script.js
+++ joguim/script.js
@@ -1,24 +1,34 @@
 const jhonatec = document.getElementById('jhonatec');
 
 const moveJhonatec = (event) => {
-  console.log(event.keyCode);
-  switch (event.keyCode) {
-    case 38:
-    case 87:
-    case 32:
-      jhonatec.className = 'jump';
-      break;
-    case 40:
-      jhonatec.className = 'down';
-      break;
+  // console.log(event.keyCode);
+  if (jhonatec.className === '') {
+    switch (event.keyCode) {
+      case 38:
+      case 87:
+      case 32:
+        jhonatec.className = 'jump';
+        break;
+      case 40:
+        jhonatec.className = 'down';
+        break;
+    }
   }
 };
 
 const restoreJhonatec = (event) => {
-  jhonatec.className = '';
+  // if (!isDown)
+    jhonatec.className = '';
 }
+
+const keyUpToDown = (event) => {
+  if(event.keyCode === 40){
+    jhonatec.className = '';
+  }
+};
 
 window.onload = () => {
   document.addEventListener('keydown', moveJhonatec);
+  document.addEventListener('keyup', keyUpToDown);
   jhonatec.addEventListener('animationend', restoreJhonatec);
 };
